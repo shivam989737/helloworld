bcdd89eadb743dd171c3e4d994fd14fdfdcbf2cd
M1050687 <Shivam9872460774@gmail.com>
2020-04-01 01:22:06 +0530

    Armstrong program done

---
+++ Armstrong.py
@@ -0,0 +1,13 @@
+#Armstrong
+num = int(input("Enter a number: "))
+len=len(str(num))
+sum = 0
+temp = num
+while temp > 0:
+   digit = temp % 10
+   sum =sum+digit ** len
+   temp //= 10
+if num == sum:
+   print(num,"is an Armstrong number")
+else:
+   print(num,"is not an Armstrong number")
--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,22 @@
-print ("Hello World")
+#sum
+a=24
+b=12
+print (a+b)
+#subtraction
+c=24
+d=20
+print (c-d)
+#Division
+e=30
+f=10
+print (e/f)
+#Multiplication
+g=40
+h=20
+print (g*h)
+
+#sum
+i=50
+j=70
+print(i+j)
+
