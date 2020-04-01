53725d0fb18cabe35ddf98e0b20bba5179a65a4e
M1050687 <Shivam9872460774@gmail.com>
2020-04-01 23:42:04 +0530

    exercise 1

---
+++ Exercise_1.py
@@ -0,0 +1,5 @@
+print("*** print these following lines ***")
+print("this is \\\\ double backlash")
+print("these are /\\/\\/\\/\\/\\ mountains")
+print("he is \tawesome")
+print("\\\" \\n \\t \\\'")
